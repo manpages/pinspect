894cca8de6b879d9af0e46321436c647daa13eed
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-04-23 07:38:17 +0300

    proper escaping; string split algo bugfix

--- lib/pinspect.ex
+++ lib/pinspect.ex
@@ -21,6 +21,53 @@ defmodule Binary.PInspect.Utils do
     length = String.length rem 
     do_split_string String.slice(rem, width, length), width, [String.slice(rem, 0, width)|acc]
   end
+
+  def escape(other, char) do
+    b = do_escape(other, char, <<>>)
+    << char, b :: binary, char >>
+  end 
+
+  @compile {:inline, do_escape: 3}
+  defp do_escape(<<>>, _char, binary), do: binary
+  defp do_escape(<< char, t :: binary >>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, char >>) 
+  end 
+  defp do_escape(<<?#, ?{, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?#, ?{ >>) 
+  end 
+  defp do_escape(<<?\a, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?a >>) 
+  end 
+  defp do_escape(<<?\b, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?b >>) 
+  end 
+  defp do_escape(<<?\d, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?d >>) 
+  end 
+  defp do_escape(<<?\e, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?e >>) 
+  end 
+  defp do_escape(<<?\f, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?f >>) 
+  end 
+  defp do_escape(<<?\n, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?n >>) 
+  end 
+  defp do_escape(<<?\r, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?r >>) 
+  end 
+  defp do_escape(<<?\\, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?\\ >>) 
+  end 
+  defp do_escape(<<?\t, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?t >>) 
+  end 
+  defp do_escape(<<?\v, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, ?\\, ?v >>) 
+  end 
+  defp do_escape(<<h, t :: binary>>, char, binary) do
+    do_escape(t, char, << binary :: binary, h >>) 
+  end
 end
 
 defimpl Binary.PInspect, for: BitString do
@@ -28,10 +75,10 @@ defimpl Binary.PInspect, for: BitString do
   def inspect(string, opts) do 
     width = Keyword.get opts, :width, :infinity
     if width == :infinity do
-      string = %b("#{string}")
+      string = escape string, ?"
     else 
-      [h|t] = Enum.reverse split_string(string, width)
-      List.foldr t, %b("#{h}"), fn(x, acc) -> %b("#{x}") <> "<>\n" <> acc end
+      [h|t] = split_string(string, width)
+      List.foldl t, escape(h, ?"), fn(x, acc) -> acc <> "<>\n" <> escape(x, ?") end
     end
   end
 end
