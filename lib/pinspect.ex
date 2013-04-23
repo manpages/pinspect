d108a86b61615df5b95f5fa2d6da9001aaedd829
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-04-23 05:45:28 +0300

    placeholders

--- lib/pinspect.ex
+++ lib/pinspect.ex
@@ -8,3 +8,35 @@ defprotocol Binary.PInspect do
 
   def inspect(thing, opts)
 end
+
+defimpl Binary.PInspect, for: BitString do
+  def inspect(_,_), do: "BitString"
+end
+
+defimpl Binary.PInspect, for: List do
+  def inspect(_,_), do: "List"
+end
+
+defimpl Binary.PInspect, for: Atom do
+  def inspect(_,_), do: "Atom"
+end
+
+defimpl Binary.PInspect, for: Number do
+  def inspect(_,_), do: "Number"
+end
+
+defimpl Binary.PInspect, for: Function do
+  def inspect(_,_), do: "Function"
+end
+
+defimpl Binary.PInspect, for: PID do
+  def inspect(_,_), do: "PID"
+end
+
+defimpl Binary.PInspect, for: Port do
+  def inspect(_,_), do: "Port"
+end
+
+defimpl Binary.PInspect, for: Reference do
+  def inspect(_,_), do: "Reference"
+end
