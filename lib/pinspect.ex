4827744c0c4bcd1570b6ededc6b8fe13fc62b78f
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-04-23 05:17:38 +0300

    protocol

--- lib/pinspect.ex
+++ lib/pinspect.ex
@@ -1,2 +1,10 @@
-defmodule Pinspect do
+defprotocol Binary.PInspect do
+  @moduledoc """
+  Draft code for pretty printing protocol.
+  To be merged with Elixir code base later.
+  """
+
+  @only [BitString, List, Tuple, Atom, Number, Function, PID, Port, Reference]
+
+  def inspect(thing, opts)
 end
