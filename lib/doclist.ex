21db7d0212748bf0b87655a393af5fec9f2d62e0
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-04-30 02:30:21 +0300

    doclist.ex

---
+++ lib/doclist.ex
@@ -0,0 +1,28 @@
+defmodule Binary.DocList do
+  @moduledoc """
+  `Binary.DocList` provides functions that take
+  Elixir terms as input and output document 
+  lists which in turn are intermediate term
+  representations for laying out the document
+  by `Binary.PInspect` implementations.
+  """
+
+  defrecord T, prefix: "", body: "", postfix: "", meta: Keyword.new()
+
+  @spec wrap(binary, binary, [T.t]) :: [T.t]
+  def wrap(left, right, [docfirst|doclines]) do
+    docfirst = docfirst.body(left <> docfirst.body)
+    [doclast|doclines] = Enum.reverse doclines
+    doclast = doclast.body(doclast.body <> right)
+    [docfirst|(Enum.reverse [doclast|doclines])]
+  end
+
+  @spec nest([T.t]) :: [T.t]
+  def nest(document) do
+    lc x inlist document do 
+      x.prefix("  " <> x.prefix).meta(
+        Keyword.put x.meta, :nesting, Keyword.get(x.meta, :nesting, 0)+1
+      )
+    end
+  end
+end
